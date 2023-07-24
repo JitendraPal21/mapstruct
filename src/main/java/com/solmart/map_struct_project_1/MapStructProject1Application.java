b9d7c2fda90636dad32cb8dc112a941f7cb9a2c4
JitendraPal21 <paljitendra21@gmail.com>
2023-07-24 08:14:09 +0530

    1st commit

---
+++ src/main/java/com/solmart/map_struct_project_1/MapStructProject1Application.java
@@ -0,0 +1,13 @@
+package com.solmart.map_struct_project_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStructProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapStructProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/solmart/map_struct_project_1/MapStructProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solmart.map_struct_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStructProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
